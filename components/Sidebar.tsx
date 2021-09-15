7342613206e3f4f3e52cba0e0bb1cfbe4375bc3c
rchang1163 <46568041+rchang1163@users.noreply.github.com>
2021-09-15 04:12:36 -0400

    Initialize Repo

---
+++ components/Sidebar.tsx
@@ -0,0 +1,84 @@
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin, AiFillMail, AiFillYoutube } from "react-icons/ai";
+import { GiTie } from "react-icons/gi";
+import { GoLocation } from "react-icons/go";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+
+const Sidebar = () => {
+  const { theme, setTheme } = useTheme();
+
+  const changeTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <>
+      <Image
+        src =  "/images/ryanchang.jpg"
+        alt="avatar"
+        className=" mx-auto border rounded-full "
+        height="128px"
+        width="128px"
+        layout="intrinsic"
+        quality="100"
+      />
+      <h3 className="my-4 text-3xl font-bold tracking-wider font-prompt">
+        <span className="text-blue ">Ryan</span> Chang
+      </h3>
+      <p className="px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200 dark:bg-black-500">
+        Software Developer
+      </p>
+      {/* Resume */}
+      <a
+        href="https://drive.google.com/file/d/1ZG01856lfUR4gDpZHd1HHNQTHhWtn_s5/view?usp=sharing"
+        className="flex items-center justify-center px-2 py-1 my-2 bg-gray-200 rounded-full cursor-pointer dark:bg-dark-200 dark:bg-black-500"
+      >
+        <GiTie className="w-6 h-6" />
+        <span>My Resume</span>
+      </a>
+
+      {/* Socials */}
+      <div className="flex justify-around w-9/12 mx-auto my-5 text-green md:w-full ">
+        <a href="https://www.linkedin.com/in/ryan-chang-25a653197/" >
+          <AiFillLinkedin className="w-8 h-8 cursor-pointer" />
+        </a>
+        <a href="https://github.com/rchang0501">
+          <AiFillGithub className="w-8 h-8 cursor-pointer" />{" "}
+        </a>
+        <a href="https://www.youtube.com/channel/UCV2Qn0HdOtAsPBEfr-lJNig">
+          <AiFillYoutube className="w-8 h-8 cursor-pointer" />
+        </a>
+      </div>
+
+      {/* Contacts */}
+      <div
+        className="py-4 my-5 bg-gray-200 dark:bg-dark-200 dark:bg-black-500"
+        style={{ marginLeft: "-1rem", marginRight: "-1rem" }}
+      >
+        <div className="flex items-center justify-center">
+          <GoLocation className="mr-2" /> <span>Toronto, Ontario </span>
+        </div>
+        <p className="my-2 "> r28chang@uwaterloo.ca </p>
+        <p className="my-2"> (416) 316-5322 </p>
+      </div>
+
+      {/* Email Button */}
+
+      <button
+        className="w-8/12 px-5 py-2 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 hover:scale-105 focus:outline-none"
+        onClick={() => window.open("mailto:r28chang@uwaterloo.ca")}
+      >
+        Email me
+      </button>
+      <button
+        onClick={changeTheme}
+        className="w-8/12 px-5 py-2 my-4 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 focus:outline-none hover:scale-105 "
+      >
+        {/* //TODO remove bg black */}
+        Switch Theme
+      </button>
+    </>
+  );
+};
+
+export default Sidebar;
---
+++ pages/resume.tsx
@@ -0,0 +1,68 @@
+import Bar from "../components/Bar";
+import { languages, tools } from "../data";
+import { motion } from "framer-motion";
+import { fadeInUp, routeAnimation } from "../animations";
+
+const Resume = () => {
+  return (
+    <motion.div
+      className="px-6 py-2"
+      variants={routeAnimation}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+    >
+      {/* //! Education & Experience */}
+      <div className="grid gap-6 md:grid-cols-2">
+        <motion.div variants={fadeInUp} initial="initial" animate="animate">
+          <h5 className="my-3 text-2xl font-bold">Education</h5>
+          <div className="">
+            <h5 className="my-2 text-xl font-bold">
+              Systems Design Engineering
+            </h5>
+            <p className="font-semibold">University of Waterloo (2021-2026)</p>
+            <p className="my-3">
+              I am currently a candidate for a Bachelor of Applied Science
+              (BASc) in Systems Design Engineering from the University of
+              Waterloo
+            </p>
+          </div>
+        </motion.div>
+        <motion.div variants={fadeInUp} initial="initial" animate="animate">
+          <h5 className="my-3 text-2xl font-bold">Experience</h5>
+          <div className="">
+            <h5 className="my-2 text-xl font-bold">Lead Front-End Developer</h5>
+            <p className="font-semibold">Ontario DECA</p>
+            <p className="my-3">
+              I redesigned and manged the website for Ontario DECA, the largest
+              DECA associaition in the world with over 15,000 members
+            </p>
+          </div>
+        </motion.div>
+      </div>
+
+      {/*Languages & Tools */}
+      <div className="grid gap-9 md:grid-cols-2">
+        <div>
+          <h5 className="my-3 text-2xl font-bold">Language & Framework</h5>
+          <div className="my-2">
+            {languages.map((language, i) => (
+              <Bar value={language} key={i} />
+            ))}
+          </div>
+        </div>
+
+        <div>
+          <h5 className="my-3 text-2xl font-bold">Tools & Softwares</h5>
+          <div className="my-2">
+            {tools.map((tool, i) => (
+              <Bar value={tool} key={i} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Resume;
